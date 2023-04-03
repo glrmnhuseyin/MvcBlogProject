2b1535337a40390dd8878da0692f0d1c6c1d316a
glrmnhuseyin <125462131+glrmnhuseyin@users.noreply.github.com>
2023-04-03 15:48:05 +0300

    Commit2

---
+++ Blog/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class CategoryController : Controller
+    {//Get:Category
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,30 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo=new GenericRepository<Category>();
+        public List<Category> GettAll() 
+        {
+            return repo.List();
+        }
+        public void CategoryAddBll(Category p)
+        {
+            if (p.CategoryName=="" || p.CategoryName.Length<=3 || p.CategoryDescription=="" || p.CategoryName.Length>=51)
+            {
+                //Hata mesajı
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+        }
+    }
+}
