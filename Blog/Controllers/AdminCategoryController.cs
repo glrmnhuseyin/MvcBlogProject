007afda0c5e09007e0d0793baa5d4627d3f607bc
glrmnhuseyin <125462131+glrmnhuseyin@users.noreply.github.com>
2023-04-04 02:16:50 +0300

    Commit 03.04.2023 son işlem

--- Blog/Controllers/AdminCategoryController.cs
+++ Blog/Controllers/AdminCategoryController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrate;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
 {
     public class AdminCategoryController : Controller
     {
+        CategoryManager categoryManager = new CategoryManager(new EFCategoryDal());
         public IActionResult Index()
         {
+            var categoryvalues=categoryManager.GetList();
+            return View(categoryvalues);
+        }
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public  IActionResult AddCategory(Category category)
+        {
+            CategoryValidator categoryvalidator=new CategoryValidator();
+            ValidationResult results = categoryvalidator.Validate(category);
+            if (results.IsValid)
+            {
+                categoryManager.CategoryAdd(category);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
     }
